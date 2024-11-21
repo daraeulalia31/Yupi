a280b9110a2bdff35409a6cf0b73f58db904e2f5
daraeulalia31 <DaraEulalia31@gmail.com>
2024-11-21 09:25:35 -0500

    Yupi

---
+++ lib/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp
